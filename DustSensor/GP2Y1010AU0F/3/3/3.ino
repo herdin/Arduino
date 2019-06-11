49e1a41b4c4abcc1b04c699c513692ac27064901
epu baal <herdin86@gmail.com>
2019-06-11 17:54:10 +0900

    mod

--- DustSensor/GP2Y1010AU0F/3/3/3.ino
+++ DustSensor/GP2Y1010AU0F/3/3/3.ino
@@ -7,6 +7,13 @@
 
 #define PURE_VOLTAGE 145;
 
+#define BUTTON_NONE   0
+#define BUTTON_SELECT 1
+#define BUTTON_LEFT   2
+#define BUTTON_DOWN   3
+#define BUTTON_UP     4
+#define BUTTON_RIGHT  5
+
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 int dustPin = 5; //a pin
@@ -25,17 +32,40 @@ int sleepTime = 9680;
 long calibrationIndex = 0;
 float calibrationVoltage = 0;
 float realVoltage = 0;
-float caclVoltage = 0;
+float calcVoltage = 0;
 float dustdensity = 0;
 float ppmpercf = 0;
 char s[32];
 
-float voMeasured = 0;
-float calcVoltage = 0;
-float dustDensity = 0;
+int read_button  = 0;
+
+int read_LCD_buttons()
+{
+  read_button = analogRead(0); //analog read from AO pin
+  Serial.print("read_button : ");
+  Serial.print(read_button);
+  Serial.print(" : ");
+  /* 1023 none
+   * 638 select
+   * 409 left
+   * 256 down
+   * 99 up
+   * 0 right
+   */
+  
+  if (read_button > 1000) return BUTTON_NONE;
+  else if(read_button > 600) return BUTTON_SELECT;
+  else if(read_button > 300) return BUTTON_LEFT;
+  else if(read_button > 200) return BUTTON_DOWN;
+  else if(read_button > 50) return BUTTON_UP;
+  else return BUTTON_RIGHT;
+}
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2); //LCD init
+  lcd.setCursor(0, 0); //LCD cursor set
+  lcd.print("Air Condition"); //LCD display
   pinMode(ledPower, OUTPUT);
 }
 
@@ -70,19 +100,24 @@ void loop() {
    * unsigned char precision_length,
    * char* charBuffer
    */
-  String dataString = "realVoltage[";
-  dataString += dtostrf(realVoltage, 10, 4, s); //9, 4
-  dataString += "] calcVoltage[";
-  dataString += dtostrf(calcVoltage, 10, 4, s); //9, 4
-  dataString += "] dustdensity[";
-  dataString += dtostrf(dustdensity, 10, 4, s); //5, 2
-  dataString += "] ppmpercf[";
-  dataString += dtostrf(ppmpercf, 10, 4, s); //8, 0
-  dataString += "] calibrationVoltage[";
-  dataString += dtostrf((calibrationVoltage/calibrationIndex), 10, 4, s);
-  dataString += "]\n";
-  
-  Serial.print(dataString);
+  String logString = "realVoltage[";
+  logString += dtostrf(realVoltage, 10, 4, s); //9, 4
+  logString += "] calcVoltage[";
+  logString += dtostrf(calcVoltage, 10, 4, s); //9, 4
+  logString += "] dustdensity[";
+  logString += dtostrf(dustdensity, 10, 4, s); //5, 2
+  logString += "] ppmpercf[";
+  logString += dtostrf(ppmpercf, 10, 4, s); //8, 0
+  logString += "] calibrationVoltage[";
+  logString += dtostrf((calibrationVoltage/calibrationIndex), 10, 4, s);
+  logString += "]\n";
+
+  String lcdString = "";
+  lcdString += dtostrf(dustdensity, 10, 4, s);
+  lcdString += "";
+  lcd.setCursor(0, 1); //LCD cursor line 0 pannel 0
+  lcd.print(lcdString);
+  Serial.print(logString);
 
   delay(1000);
 }
--- LiquidCrystal/DFR0009/1/1.ino
+++ LiquidCrystal/DFR0009/1/1.ino
@@ -8,8 +8,8 @@
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-int lcd_key     = 0;
-int pinRead  = 0;
+int read_lcd_key  = 0;
+int read_button  = 0;
 
 #define BUTTON_NONE   0
 #define BUTTON_SELECT 1
@@ -20,9 +20,9 @@ int pinRead  = 0;
 
 int read_LCD_buttons()
 {
-  pinRead = analogRead(0); //analog read from AO pin
-  Serial.print("pinRead : ");
-  Serial.print(pinRead);
+  read_button = analogRead(0); //analog read from AO pin
+  Serial.print("read_button : ");
+  Serial.print(read_button);
   Serial.print(" : ");
   /* 1023 none
    * 638 select
@@ -32,11 +32,11 @@ int read_LCD_buttons()
    * 0 right
    */
   
-  if (pinRead > 1000) return BUTTON_NONE;
-  else if(pinRead > 600) return BUTTON_SELECT;
-  else if(pinRead > 300) return BUTTON_LEFT;
-  else if(pinRead > 200) return BUTTON_DOWN;
-  else if(pinRead > 50) return BUTTON_UP;
+  if (read_button > 1000) return BUTTON_NONE;
+  else if(read_button > 600) return BUTTON_SELECT;
+  else if(read_button > 300) return BUTTON_LEFT;
+  else if(read_button > 200) return BUTTON_DOWN;
+  else if(read_button > 50) return BUTTON_UP;
   else return BUTTON_RIGHT;
 }
 
@@ -55,8 +55,8 @@ void loop()
   lcd.print(millis() / 1000); //LCD display
 
   lcd.setCursor(0, 1); //LCD cursor line 1 pannel 0
-  lcd_key = read_LCD_buttons(); //button read
-  switch (lcd_key)
+  read_lcd_key = read_LCD_buttons(); //button read
+  switch (read_lcd_key)
   {
     case BUTTON_RIGHT:
         lcd.print("RIGHT");//1
